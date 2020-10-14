9cb5ed91e05152ce6245097530297e9093a0c3bd
tarunraghav226 <tarun.raghav226@gmail.com>
2020-10-14 14:44:04 +0530

    Added static root directory.

--- InstantShop/InstantShop/settings.py
+++ InstantShop/InstantShop/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'allstatic'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
