7cdca0ba2ebde45ce4086e5a21213c0c2cb24a31
tarunraghav226 <tarun.raghav226@gmail.com>
2020-10-14 14:37:00 +0530

    Updated allowed hosts in settings file.

--- InstantShop/InstantShop/settings.py
+++ InstantShop/InstantShop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7&!@wovf4jqqez^g5*yno=^3^8_tq96wzkg89g$wffhmiwokpt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['instantshop.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
