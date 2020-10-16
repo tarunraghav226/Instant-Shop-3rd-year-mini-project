f3ed0dd230d69b6f65e9d0b98678242e903d9036
tarunraghav226 <tarun.raghav226@gmail.com>
2020-10-16 18:05:34 +0530

    Added customer user model.

--- InstantShop/user/models.py
+++ InstantShop/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+def user_directory(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.username,filename)
+
+class CustomerUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField()
+    dob = models.DateField()
+    phone_number = models.TextField(max_length=13)
+    photo = models.FileField(upload_to = user_directory)
+
+    def __str__(self):
+        return str(self.user.username)
