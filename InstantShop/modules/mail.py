91dbe8496da607dcf8bafa90634a476315909f6c
Tarun <49776598+tarunraghav226@users.noreply.github.com>
2020-10-18 13:59:11 +0530

    Updated link in mail.py module.

--- InstantShop/modules/mail.py
+++ InstantShop/modules/mail.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from InstantShop import settings
 
 def mail(email, firstname, link):
-    link = 'http://127.0.0.8000/verify-email/' + link
+    link = 'http://instantshop.pythonanywhere.com/verify-email/' + link
     subject = 'Email Verification Mail.'
     message = 'Dear {0},\n\t\tCongratulations, you have succesfully registerd to InstantShop.\nBelow is the link to verify your email. Click it to verify your mail.\n{1}'.format(firstname,link)
     recepient = email
