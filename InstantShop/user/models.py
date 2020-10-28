59fe96c0b354e3272eb2f73eec342e3d34494dff
tarunraghav226 <tarun.raghav226@gmail.com>
2020-10-28 15:26:19 +0530

    Added product model.

--- InstantShop/user/models.py
+++ InstantShop/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
@@ -17,3 +18,21 @@ class CustomerUser(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+class Products(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 40)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 5, decimal_places = 2) 
+    product_uploaded_on_date = models.DateField(default=datetime.date.today())
+    selled = models.BooleanField(default=False)
+    features = models.TextField()
+    months_of_product_used = models.IntegerField()
+
+    img1 = models.ImageField(upload_to = user_directory ,blank=True)
+    img2 = models.ImageField(upload_to = user_directory ,blank=True)
+    img3 = models.ImageField(upload_to = user_directory ,blank=True)
+    img4 = models.ImageField(upload_to = user_directory ,blank=True)
+
+    def __str__(self):
+        return self.name+" "+self.user.username
