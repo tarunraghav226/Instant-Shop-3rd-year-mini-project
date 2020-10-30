dbd38d12a9ee7c51773da49bf5c07bee5c4963c2
tarunraghav226 <tarun.raghav226@gmail.com>
2020-10-30 14:24:39 +0530

    Added url and view for editing a product.

--- InstantShop/InstantShop/urls.py
+++ InstantShop/InstantShop/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('previous-orders/', view.PreviousOrderDetailsView.as_view(), name='previous-orders'),
     path('uploaded-products/', view.UploadedProductsView.as_view(), name='uploaded-products'),
     path('delete/<int:id>', view.DeleteProductView.as_view(), name='delete'),
+    path('edit/<int:id>', view.EditProductView.as_view(), name='edit'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
